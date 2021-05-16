93ebd442db8835888c76ee5c9a99c30ac665f515
Daniel Scott <danielrobertscott@outlook.com>
2021-05-16 14:41:43 +0100

    Progress with auto tnt placement

--- testing/Mission2Tnt.lua
+++ testing/Mission2Tnt.lua
@@ -1,44 +1,89 @@
 assert(loadfile("../gameAddresses.lua"))()
 
-local positions = {}
-local position1From = -4620
-local position1To = -4580
-local position2From = -5120
-local position2To = -5080
-
-for pos1=position1From,position1To do
-    for pos2=position2From,position2To do
-        positions[#positions+1]= { pos1, pos2 }
+local function getRange(from, to, increment)
+    local values={}
+    for value=math.min(from, to),math.max(from, to),math.abs(increment) do
+        values[#values+1]= value
     end
+    if (#values == 0) then
+        values[#values+1]= from
+    end
+    return values
 end
 
-for k, position in pairs(positions) do
-    savestate.load("SaveStates/Mission2Tnt.State")
+local function getPositions(from1, to1, from2, to2, increment1, increment2)
+    local positions={}
+    local range1 = getRange(from1, to1, increment1)
+    local range2 = getRange(from2, to2, increment2)
+    for kR1, pos1 in pairs(range1) do
+        for kR2, pos2 in pairs(range2) do
+            positions[#positions+1]= { pos1, pos2 }
+        end
+    end
+    return positions
+end
 
-    --SetToTntPos
-    memory.write_s16_le(gameAddresses.mission2.p1_rotation, 2610)
-    memory.write_s16_le(gameAddresses.mission2.p1_position_1, -7908)
-    memory.write_s16_le(gameAddresses.mission2.p1_position_2, -6531)
-    emu.frameadvance()
+local restPositions = getPositions(-4600, -4600, -5100, -5100, 5, 5)
+local startPositions = getPositions(-7880, -7880,  -6580, -6580, 5, 5)
+local startRotations = getRange(2550, 2680, 5)
 
-    --DropTnt
-    joypad.set({Cross=true}, 1)
-    emu.frameadvance()
-    joypad.set({Cross=true}, 1)
-    emu.frameadvance()
-    joypad.set({Cross=false}, 1)
-    for i=1,90 do emu.frameadvance() end
-    --
-    --Walk Past Enemy
-    memory.write_s16_le(gameAddresses.mission2.p1_rotation, 792)
-    memory.write_s16_le(gameAddresses.mission2.p1_position_1, -7368)
-    memory.write_s16_le(gameAddresses.mission2.p1_position_2, -6323)
-    for i=1,90 do emu.frameadvance() end
+console.log("Complexity: " .. #restPositions * #startPositions * #startRotations)
+console.log("#restPositions: " .. #restPositions)
+console.log("#startPositions: " .. #startPositions)
+console.log("#startRotations: " .. #startRotations)
 
-    --SetToRestPos
-    memory.write_s16_le(gameAddresses.mission2.p1_rotation, 836)
-    memory.write_s16_le(gameAddresses.mission2.p1_position_1, position[1])
-    memory.write_s16_le(gameAddresses.mission2.p1_position_2, position[2])
+for kReset, restPosition in pairs(restPositions) do
+    for kStart, startPosition in pairs(startPositions) do
+        for kStartRot, startRotation in pairs(startRotations) do
+            savestate.load("SaveStates/Mission2Tnt.State")
 
-    for i=1,1200 do emu.frameadvance() end
+            console.log("");
+            console.log("start rotation:"..startRotation);
+            console.log("start position:"..startPosition[1].." "..startPosition[2]);
+            console.log("rest position:"..restPosition[1].." "..restPosition[2]);
+
+            --SetToTntPos
+            memory.write_s16_le(gameAddresses.mission2.p1_rotation, startRotation)
+            memory.write_s16_le(gameAddresses.mission2.p1_position_1, startPosition[1])
+            memory.write_s16_le(gameAddresses.mission2.p1_position_2, startPosition[2])
+            emu.frameadvance()
+
+            --DropTnt
+            joypad.set({Cross=true}, 1)
+            emu.frameadvance()
+            joypad.set({Cross=true}, 1)
+            emu.frameadvance()
+            for i=1,90 do emu.frameadvance() end
+            --
+            --Walk Past Enemy
+            memory.write_s16_le(gameAddresses.mission2.p1_rotation, 792)
+            memory.write_s16_le(gameAddresses.mission2.p1_position_1, -7368)
+            memory.write_s16_le(gameAddresses.mission2.p1_position_2, -6323)
+            for i=1,90 do emu.frameadvance() end
+
+            --SetToRestPos
+            memory.write_s16_le(gameAddresses.mission2.p1_rotation, 836)
+            memory.write_s16_le(gameAddresses.mission2.p1_position_1, restPosition[1])
+            memory.write_s16_le(gameAddresses.mission2.p1_position_2, restPosition[2])
+            emu.frameadvance()
+
+            --Move Back and Forward to get valid height
+            joypad.set({Down=true}, 1)
+            emu.frameadvance()
+            joypad.set({Down=true}, 1)
+            emu.frameadvance()
+            joypad.set({Up=true}, 1)
+            emu.frameadvance()
+            joypad.set({Up=true}, 1)
+            emu.frameadvance()
+
+            --SetBackToRestPos
+            memory.write_s16_le(gameAddresses.mission2.p1_rotation, 836)
+            memory.write_s16_le(gameAddresses.mission2.p1_position_1, restPosition[1])
+            memory.write_s16_le(gameAddresses.mission2.p1_position_2, restPosition[2])
+            emu.frameadvance()
+
+            for i=1,1200 do emu.frameadvance() end
+        end
+    end
 end
